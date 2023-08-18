d881ba6f17b5d5fdac72bd5b3dbb0033071d49c0
Hassen Hassen <xasansq@gmail.com>
2023-08-18 09:48:54 -0500

    async patterns

--- NODE/16-promises.js
+++ NODE/16-promises.js
@@ -1,19 +1,37 @@
 // Promises - Cleaner way to write the code in 11-fs-async.js
 
-const { readFile } = require('fs')
+const { readFile, writeFile } = require('fs').promises
 
-const getText = (path) => {
-    return new Promise((resolve, reject) => {
-        readFile(path, 'utf-8', (err, result) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(result)
-            }
-        })
-    })
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
+
+
+const start = async () => {
+    try {
+        const first = await readFile('./content/first.txt', 'utf-8')
+        const second = await readFile('./content/second.txt', 'utf-8')
+        await writeFile('./content/result-mind-grenade.txt', `Yuh: ${first}, ${second}`)
+        console.log(first, second);
+    } catch (err) {
+        console.log(err);
+    }
 }
+start()
 
-getText('./content/first.txt')
-    .then(result => console.log(result))
-    .catch((err) => console.log(err))
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf-8', (err, result) => {
+//             if (err) {
+//                 reject(err)
+//             } else {
+//                 resolve(result)
+//             }
+//         })
+//     })
+// }
+
+
+// getText('./content/first.txt')
+//     .then(result => console.log(result))
+//     .catch((err) => console.log(err))
