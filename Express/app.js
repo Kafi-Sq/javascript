c37cc5f9805b5d957ee3d3f3cdb993d97f6c9ea1
Hassen <xasansq@gmail.com>
2023-09-07 10:15:18 -0500

    http module

--- Express/app.js
+++ Express/app.js
@@ -1,7 +1,20 @@
-const http = require('http')
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.end('Hello World')
-})
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.write("<h1>Hello World</h1>");
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>About World</h1>");
+    res.end();
+  }else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>404 Not found</h1>");
+    res.end();
+  }
+});
 
-server.listen(5000)
+server.listen(5000);
